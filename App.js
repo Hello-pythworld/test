5e69cc3f37da910b70f9dff4a63f2e2272e7f4c7
LeeYeongHun <yeonghun4051@daum.net>
2025-08-31 09:29:14 +0900

    real

--- App.js
+++ App.js
@@ -1,35 +1,29 @@
 function Square({ value }) {
   function handleClick() {
-    console.log('clicked!');
-}
+    console.log("clicked!");
+  }
+
   return (
-  <button
-    className="square"
-    onClick={handleClick}
-  >
-    {value}
-  </button>
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
   );
 }
 
 export default function Board() {
+  // 1~9 숫자 배열 만들기
+  const squares = Array.from({ length: 9 }, (_, i) => i + 1);
+
   return (
     <>
-    <div className="board-row">
-    <Square value ="1"/>  
-    <Square value ="2"/>
-    <Square value ="3"/>
-    </div>
-    <div className="board-row">
-    <Square value ="4"/>  
-    <Square value ="5"/>
-    <Square value ="6"/>
-    </div>
-    <div className="board-row">
-    <Square value ="7"/>  
-    <Square value ="8"/>
-    <Square value ="9"/>
-    </div>
+      {/* 3개씩 끊어서 줄(row) 만들기 */}
+      {[0, 3, 6].map((start) => (
+        <div className="board-row" key={start}>
+          {squares.slice(start, start + 3).map((num) => (
+            <Square value={num} key={num} />
+          ))}
+        </div>
+      ))}
     </>
   );
 }
