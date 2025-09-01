26724b7783c78fe82e6b7f0181e7d63512737936
LeeYeongHun <yeonghun4051@daum.net>
2025-09-01 09:41:38 +0900

    box

--- App.js
+++ App.js
@@ -1,29 +1,40 @@
-function Square({ value }) {
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log("clicked!");
+    setValue('X');
   }
 
   return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
+    <button
+      className="square"
+      onclick={handleClick}
+      >
+        {value}
+      </button>
   );
 }
 
 export default function Board() {
-  // 1~9 숫자 배열 만들기
-  const squares = Array.from({ length: 9 }, (_, i) => i + 1);
-
   return (
     <>
-      {/* 3개씩 끊어서 줄(row) 만들기 */}
-      {[0, 3, 6].map((start) => (
-        <div className="board-row" key={start}>
-          {squares.slice(start, start + 3).map((num) => (
-            <Square value={num} key={num} />
-          ))}
-        </div>
-      ))}
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
     </>
   );
 }
