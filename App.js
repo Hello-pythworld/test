7bb2d31c4ff7503cd6507fe7a05ddad2feafc925
LeeYeongHun <yeonghun4051@daum.net>
2025-09-06 13:53:41 +0900

    coding

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 export default function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fps.jpg';
+  const description = 'Gregorio Y. Zara';
   return(
   <img
     className="avatar"
-    src="https://i.imgur.com/7vQD0fps.jpg"
-    alt="Gregorio Y. Zara"
+    src={avatar}
+    alt={description}
     />
   );
 }
