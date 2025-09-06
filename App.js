bc7716b99d5016c914f7694e533c8acc3faccb9f
LeeYeongHun <yeonghun4051@daum.net>
2025-09-06 13:43:08 +0900

    main

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import Gallery from './Gallery.js';
-import { Profile } from './Gallery.js';
-
-export default function App() {
-    return (
-      <div>
-        <Profile/>
-      </div>
-    );
+export default function TodoList() {
+  return (
+    <>
+    <h1>Hedy Lamaar's Todos</h1>
+    <img
+     src="https://i.imgur.com/yXOvdOSs.jpg"
+     alt="Hedy Lammar"
+     className="photo"
+     />
+    <ul>
+      <li>Invent new traffic lights</li>
+      <li>Rehearse a movie scene</li>
+      <li>Improve the spectrum technology</li>
+    </ul>
+  </>
+  );
 }
