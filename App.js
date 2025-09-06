66c52306a1b2e3811dbc6fc12ee010da7014891b
LeeYeongHun <yeonghun4051@daum.net>
2025-09-06 17:07:52 +0900

    mistake

--- App.js
+++ App.js
@@ -1,35 +1,23 @@
-import {getImageUrl} from '/.utils.js';
+import Avatar from './Avatar.js';
 
-function Avatar(props) {
+function Card({children}) {
   return (
-    let person = props.person;
-    let size = props.size;
+    <div className="card">
+      {children}
+    </div>
+  );
 }
 
 export default function Profile() {
   return (
-    <div>
-      <Avatar
-      size={100}
-      person={{
-        name:'Katsuko Saruhashi',
-        Imageid:'Yfe0qp2'
-      }}
-      />
+    <Card>
     <Avatar
-      size={80}
-      person={{
-      name:'Akalilu Lemma'
-      imageId: 'OKS67lh'
-      }}
-      />
-      <Avatar
-      size={50}
-      person={{
-        name= 'Lin Lanying',
-        imageId:'1bX5QH6'
-      }}
-      />
-    </div>
+    size={100}
+    person={{
+      name: 'Katsuko Saruhashi',
+      imageId: 'YfeOqp2'
+    }}
+    />
+    </Card>
   );
 }
