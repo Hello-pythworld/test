2ee5f4e71aa288dc52e35347c553416f06875ea0
LeeYeongHun <yeonghun4051@daum.net>
2025-09-06 15:28:03 +0900

    everyday

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
-export default function Avatar() {
-  const avatar = 'https://i.imgur.com/7vQD0fps.jpg';
-  const description = 'Gregorio Y. Zara';
-  return(
-  <img
-    className="avatar"
-    src={avatar}
-    alt={description}
-    />
+export default function TodoList() {
+  const name = 'Gregorio Y. Zara';
+  return (
+    <h1>To Do List for {formatDate(today)}</h1>
   );
 }
