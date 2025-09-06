6ba482e9fed401ec52228f012da767356f660f88
LeeYeongHun <yeonghun4051@daum.net>
2025-09-06 16:51:32 +0900

    miss

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
-function Avatar() {
+import {getImageUrl} from '/.utils.js';
+
+function Avatar(props) {
   return (
-  <img
-    className="avatar"
-    src="https://i.imgur.com/1bX5QH6.jpg"
-    alt="Lin Lanying"
-    width={100}
-    height={100}
-    />
-  );
+    let person = props.person;
+    let size = props.size;
 }
 
 export default function Profile() {
   return (
-    <Avatar />
+    <div>
+      <Avatar
+      size={100}
+      person={{
+        name:'Katsuko Saruhashi',
+        Imageid:'Yfe0qp2'
+      }}
+      />
+    <Avatar
+      size={80}
+      person={{
+      name:'Akalilu Lemma'
+      imageId: 'OKS67lh'
+      }}
+      />
+      <Avatar
+      size={50}
+      person={{
+        name= 'Lin Lanying',
+        imageId:'1bX5QH6'
+      }}
+      />
+    </div>
   );
 }
