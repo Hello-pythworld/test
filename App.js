f2ffde31683607e722c928e9f13d629512c2c974
LeeYeongHun <yeonghun4051@daum.net>
2025-09-06 16:08:18 +0900

    cool

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday:'long' }
+  ).format(date);
+}
+
 export default function TodoList() {
-  const name = 'Gregorio Y. Zara';
   return (
     <h1>To Do List for {formatDate(today)}</h1>
   );
