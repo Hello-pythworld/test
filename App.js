759141adeb9a6b220ec829d5fa7a00a122904ffc
LeeYeongHun <yeonghun4051@daum.net>
2025-09-07 22:14:20 +0900

    chaser

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-function Item ({ name, isPacked }) {
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">{name} ✅</li>;
+}
   return <li className="item">{name}</li>;
 }
 
@@ -8,18 +11,18 @@ export default function PackingList() {
       <h1>Sally Ride's Packing List</h1>
       <ul>
         <Item
-        isPacked={true}
-        name="Space suit"
-    />
-    <Item
-      isPacked={true}
-      name="Helmet with a golden leaf"
-      />
-      <Item
-        isPacked={false}
-        name="Photo of Tam"
-        />
-        </ul>
-        </section>
+          isPacked={true}
+          name="Space suit"
+          />
+        <Item
+            isPacked={true}
+            name="Helmet with a golden leaf"
+            />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+          />
+      </ul>
+    </section>
   );
 }
