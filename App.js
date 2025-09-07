8ac139824824488a77fe75cc9e7b1984a3f68200
LeeYeongHun <yeonghun4051@daum.net>
2025-09-07 21:29:24 +0900

    Sunday

--- App.js
+++ App.js
@@ -1,23 +1,56 @@
-import Avatar from './Avatar.js';
+import { getImageUrl } from './utils.js';
 
-function Card({children}) {
+export default function Gallery() {
   return (
-    <div className="card">
-      {children}
-    </div>
-  );
-}
-
-export default function Profile() {
-  return (
-    <Card>
-    <Avatar
-    size={100}
-    person={{
-      name: 'Katsuko Saruhashi',
-      imageId: 'YfeOqp2'
-    }}
-    />
-    </Card>
+    <div>
+      <h1>Notable Scientists</h1>
+      <section className="profile">
+        <h2>Maria Skłodowska-Curie</h2>
+        <img
+          className="avatar"
+          src={getImageUrl('szV5sdG')}
+          alt="Maria Skłodowska-Curie"
+          width={70}
+          height={70}
+          /> 
+          <ul>
+            <li>
+              <b>Profession: </b>
+              physicist and chemist
+            </li>
+            <li>
+            <b>Awards: 4 </b>
+            (Nobel Prize in physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
+            </li>
+            <li>
+              <b>Discovered:</b>
+            polonium (chemical element)
+            </li>
+            </ul>
+            </section>
+            <section className="profile">
+            <h2>Katsuko Saruhashi</h2>
+            <img
+              className="avatar"
+              src={getImageUrl('Yfe0qp2')}
+              alt="Katsuko Saruhashi"
+              width={70}
+              />
+              <ul>
+                <li>
+                  <b>Profession: </b>
+                geochemist
+                </li>
+                <li>
+                  <b>Awards: 2</b>
+                  (Miyake Prize for geochemistry, Tanaka Prize)
+                </li>
+                <li>
+                  <b>Discovered: </b>
+                  a method for measuring carbon dioxide in seawater
+                </li>
+              </ul>
+              </section>
+          </div>
   );
 }
