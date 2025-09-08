c6e447bb77a04d4f6c34f217407482922da672f8
LeeYeongHun <yeonghun4051@daum.net>
2025-09-08 13:27:50 +0900

    easy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // App 1 또는 App 2 중 원하는 것만 import
-import PackingListOne from './App-6';
+import PackingListOne from './App-7';
 // import PackingListTwo from './app-2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
